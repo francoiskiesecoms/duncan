398eb032276db1c2cb318522f146479825849004
Francois Kiesecoms <kiesecoms.francois@gmail.com>
2019-04-23 21:24:33 +0200

    fixed banner

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,9 +4,6 @@ class BookingsController < ApplicationController
     @booking = Booking.new
     @booking.dog = Dog.find_by(id:booking_params['dog'])
     @booking.session = Session.find_by(id:booking_params['class'])
-
-    # get the dog id, and assign it to booking
-    # experience of the dog will be built in model and correspond to the completed classes * score
     @booking.user = current_user
     if @booking.save
       redirect_to upcoming_classes_path
@@ -17,7 +14,6 @@ class BookingsController < ApplicationController
 
   def destroy
     Booking.find(params[:id]).destroy
-
   end
 
   private
